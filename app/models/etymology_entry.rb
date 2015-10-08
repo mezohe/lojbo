d0e0fd3d6f517dd1f05ef26c70a67ddf56095eb5
russelleh <russell.west.eh@gmail.com>
2015-10-07 19:41:19 -0600

    Add wiktionary link

--- app/models/etymology_entry.rb
+++ app/models/etymology_entry.rb
@@ -1,8 +1,11 @@
 class EtymologyEntry
-  attr_reader :language, :roman
+  attr_reader :language, :roman, :wiktionary_link
 
   def initialize(etymology)
     @language = etymology.language.iso_code
     @roman = etymology.roman
+    code = etymology.language.iso_code
+    script = etymology.etymology
+    @wiktionary_link = "https://#{code}.wiktionary.org/wiki/#{script}"
   end
 end
